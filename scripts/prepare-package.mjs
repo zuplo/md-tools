877e56a1d675003a7a5d7bfe5df06d274bd7c88b
Nate Totten <nate@zuplo.com>
2022-08-12 17:17:52 -0400

    updated package release

---
+++ scripts/prepare-package.mjs
@@ -0,0 +1,37 @@
+import {
+  copyFile,
+  copyFileSync,
+  existsSync,
+  readFileSync,
+  writeFileSync,
+} from "fs";
+import { resolve } from "path";
+import { format } from "prettier";
+const githubRef = process.argv[2];
+
+const outPkgPath = resolve("./dist/package.json");
+
+const pkgPath = resolve("./package.json");
+
+const json = readFileSync(pkgPath, "utf-8");
+const pkg = JSON.parse(json);
+const version = githubRef.replace("refs/tags/v", "");
+pkg.version = version;
+
+delete pkg.scripts;
+delete pkg.type;
+delete pkg.dependencies;
+delete pkg.devDependencies;
+
+const outJson = JSON.stringify(pkg);
+const formattedJson = format(outJson, {
+  parser: "json-stringify",
+});
+writeFileSync(outPkgPath, formattedJson, "utf-8");
+
+copyFileSync(resolve("./.npmignore"), resolve("./dist/.npmignore"));
+copyFileSync(resolve("./.npmrc"), resolve("./dist/.npmrc"));
+copyFileSync(
+  resolve("./package-lock.json"),
+  resolve("./dist/package-lock.json")
+);
